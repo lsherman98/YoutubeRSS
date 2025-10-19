521b66baee61b36a2fb7c8fafe6ebbcbae37f4dc
lsherman98 <levisherman98@gmail.com>
2025-10-19 02:56:50 -0400

    mail hooks

--- pocketbase/pb_hooks/mailer_hooks/main.go
+++ pocketbase/pb_hooks/mailer_hooks/main.go
@@ -2,14 +2,16 @@ package mailer_hooks
 
 import (
 	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/core"
 )
 
 func Init(app *pocketbase.PocketBase) error {
-	app.OnMailerSend().BindFunc(func(e *core.MailerEvent) error {
-		e.Message.Headers["Reply-To"] = "levisherman98@gmail.com"
-		return e.Next()
-	})
+	// app.OnMailerSend().BindFunc(func(e *core.MailerEvent) error {
+	// 	if e.Message.Headers == nil {
+	// 		e.Message.Headers = make(map[string]string)
+	// 	}
+	// 	e.Message.Headers["Reply-To"] = "levisherman98@gmail.com"
+	// 	return e.Next()
+	// })
 
 	return nil
 }
